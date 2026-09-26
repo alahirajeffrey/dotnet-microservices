5a8f5f4e042763bd833c0d65ac0f8a2f6b0ea207
alahirajeffrey <alahirajeffrey@gmail.com>
2026-09-26 12:11:41 +0100

    removed private field to allow for automatic admin creation

--- AuthService/Models/User.cs
+++ AuthService/Models/User.cs
@@ -14,7 +14,7 @@ public class User
     public required string PasswordHash {get; set;} 
     public required string FirstName {get; set;} 
     public required string LastName {get; set;}
-    public Role Role {get; private set;} = Role.User; // private field restricts modification
+    public Role Role {get; set;} = Role.User;
     public DateTime CreatedAt {get; private set;} = DateTime.UtcNow;
 
 }
