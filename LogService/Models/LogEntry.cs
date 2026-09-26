1dff9719e078f91f0b9d941176869222c5eecc86
alahirajeffrey <alahirajeffrey@gmail.com>
2026-09-26 12:15:14 +0100

    code refactor

--- LogService/Models/LogEntry.cs
+++ LogService/Models/LogEntry.cs
@@ -1,4 +1,3 @@
-using MongoDB.Bson;
 using MongoDB.Bson.Serialization.Attributes;
 
 namespace LogService.Models;
@@ -6,14 +5,13 @@ namespace LogService.Models;
 public class LogEntry
 {
     [BsonId]
-    [BsonRepresentation(BsonType.ObjectId)]
-    public string Id { get; set; } = ObjectId.GenerateNewId().ToString();
+    public string Id { get; set; } = Guid.NewGuid().ToString("N");
 
     public string ServiceName { get; set; } = string.Empty;
     public string EventName { get; set; } = string.Empty;
     public string Message { get; set; } = string.Empty;
     public string Level { get; set; } = "Information";
     public string? UserId { get; set; }
-    public object? Details { get; set; }
+    public string? Details { get; set; }
     public DateTime Timestamp { get; set; } = DateTime.UtcNow;
 }
